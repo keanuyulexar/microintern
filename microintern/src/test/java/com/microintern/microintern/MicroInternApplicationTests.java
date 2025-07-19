7dd8d500f2eb2ed60fb3d604a31941d26a4b18c9
keanuyulexar <keanuyule764@gmail.com>
2025-07-19 12:56:02 +0200

    first commit

---
+++ microintern/src/test/java/com/microintern/microintern/MicroInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microintern.microintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
